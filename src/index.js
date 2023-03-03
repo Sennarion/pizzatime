1271a4e4c8d9fa4117dd5ccca13d7c6dec106dd9
Serhii Reznichenko <81872621+Sennarion@users.noreply.github.com>
2023-03-03 12:51:52 +0200

    change gh homepage

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import 'styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="ecommerce">
+    <BrowserRouter basename="pizzatime">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <CssBaseline>
